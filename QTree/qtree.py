2fb089710e26c9d3243562cffde4f5d50e1bd631
danielaespino <damadore2100@alumno.ipn.mx>
2023-06-05 12:58:12 -0600

    Paradigmas de Programación

---
+++ QTree/qtree.py
@@ -0,0 +1,43 @@
+mport random
+import matplotlib.pyplot as plt # biblioteca para graficar
+import matplotlib.patches as patches
+
+class Particula():
+    def __init__(self, x:float, y:float):
+        self.x = x
+        self.y = y
+
+class Nodo():
+    def __init__(self, x0:float, y0:float, w:float, h:float, particulas):
+        self.x0 = x0
+        self.y0 = y0
+        self.ancho = w
+        self.alto = h
+        self.particulas = particulas
+        self.hijos = []
+
+    def get_ancho(self):
+        return self.ancho
+
+    def get_alto(self):
+        return self.alto
+
+    def get:particulas(self):
+        return self.particulas
+
+def subdivision_recursiva(nodo:Nodo, k:int):
+   if len(nodo.particulas)<=k:
+       return
+
+   w_ = float(0.5*nodo.ancho)
+   h_ = float(0.5*nodo.alto)
+
+   p = cuantas_contiene(nodo.x0, nodo.y0, w_, h_, nodo.particulas)
+   nodo.x1 = Nodo(nodo.x0, nodo.y0, w_, h_, p)
+   subdivision_recursiva(nodo.x1, k)
+
+   p = cuantas_contiene(nodo.x0, nodo.y0+h, w_, h_, nodo.particulas)
+   nodo.x2 = Nodo(nodo.x0, nodo.y0+h, w_, h_, p)
+   subdivision recursiva(nodo.x2, k)
+
+
