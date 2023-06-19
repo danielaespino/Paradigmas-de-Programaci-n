3ed546d7d26cd0e8a9df3b6304cd86ff975c27cf
danielaespino <damadore2100@alumno.ipn.mx>
2023-06-19 13:02:00 -0600

    Paradigmas de Programacion

--- calor2Dcuda.py
+++ calor2Dcuda.py
@@ -30,5 +30,56 @@ print("celdas = ",nt)
 
 @jit
 def evolucion(u,n_0,n_1,udx2_0,udx2_!,dt,kd.i):
+    jp1 = i + n_0
+    jm1 = i - n_0
+    laplaciano = (u[i-1]-2.0*u[i]+u[i+1])*udx2_0 + (u[jm1]-2.0*u[i]+u[jp1])*udx2_1
+    unueva = u[i] + dt*kd*laplaciano
+    return unueva
 
+evoluvion_gpu = cuda.jit(device=True)(evolucion)
+
+@cuda.jit
+def solucion_kernel(u_d,un_d,udx2_0,udx2_1,dt,n_0,n_1,kd):
+    ii, jj = cuda.grid(2)
+    i = ii + n_0*jj
+    if ii==0 or jj==0 or ii==n_0-1 or jj==n_1-1:
+       unueva = 0.0
+    else:
+       unueva = evolucion_gpu(u_d,n_0,n_1,udx2_0,udx2_1,dt,kd,i)
+    if i == int((n_0*n_1)/2)+int(n_0/2):
+       unueva = 1.0
+    un_d[i] = unueva
+
+blockdim = (32,16) # hilos por bloque
+griddim = (int(n[0]/blockdim[0]),int(n[1]/blockdim[1])) #bloques en el dominio
+
+#======================
+# PROGRAMA PRINCIPAL
+#======================
+start = time.time()
+# Llenar la solución con ceros
+u = np.zeros(nt,dtype=np.float64) # arreglo de lectura
+un = np.zeros(nt,dtype=np.float64) # arreglo de escritura
+# Pasar arreglos al GPU
+u_d = cuda.to_device(u)
+un_d = cuda.to_device(un)
+# Integrar en el tiempo
+for t in range(1,pasos+1):
+    solucion_kernel[griddim,blockdim](u_d,un_d,udx2[0],udx2[1],dt,n[0],n[1],kd)
+    u_d = cuda.to_device(un_d)
+    if t%100==0: print("paso = ",t)
+# Pasar arreglo al CPU
+u_d.copy_to_host(u)
+end = time.time()
+print("Tardó: ",end-start,"s")
+#---------------------------------------------------------
+
+#----------------------------
+# Graficar en 3D
+#----------------------------
+u = np.reshape(u,(n[0],n[1]))
+x,y = np.meshgrid(np.arange(0,L[0],dx[0]),np.arange(0,L[1],dx[1]))
+ax = plt.axes(projection='3d')
+ax.plot_surface(x,y,u,cmap=cm.hsv)
+plt.show()
 
